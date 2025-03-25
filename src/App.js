165cbe09afed8113d9c5ede9ab9b8d525b48d44e
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-03-25 23:30:33 +0200

    Add initial packing list with items and Item component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Changer", quantity: 1, packed: false },
+];
+    
     export default function App() {
 
      return (
@@ -25,9 +32,23 @@
 
      function PackingList() {
         return (
-          <div className="list">LIST</div>
-        )
-      };
+          <div className="list">
+          <ul>
+            {initialItems.map((item) => (
+            <Item item={item}  />
+            ))}
+          </ul>
+          </div>
+        )};
+
+      function Item({ item }) {
+        return (
+            <li>
+              <span style={item.packed ? {textDecoration: "line-through"} : {}}>{item.quantity} {item.description}</span>
+              <button>❌</button>
+            </li>
+        );
+      }
 
        function Stats() {
         return(
