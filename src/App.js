83711e4a71ed98fd1d3acba320e77aad4b4af1fc
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-03-25 22:59:42 +0200

    Initialize main app structure with Logo, Form, PackingList, and Stats components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-function App() {
-  return (
-    <div>
-      Hallo
-    </div>
-  );
-}
+    export default function App() {
 
-export default App;
+     return (
+      <div className="app">
+         <Logo />
+        <Form />
+        <PackingList />
+        <Stats />
+      </div>
+     )
+
+    };
+
+     function Logo() {
+        return <h1>🏝️ Far Away 🧳</h1>;
+      };
+
+       function Form() {
+        return (
+          <div className="add-form">
+            <h3>What do you need for your 😍 trip?</h3>
+          </div>
+        )
+      };
+
+     function PackingList() {
+        return (
+          <div className="list">LIST</div>
+        )
+      };
+
+       function Stats() {
+        return(
+          <footer className="">
+         <em>
+         💼 You have X items on your list, and you already packed X (X%)
+         </em>
+        </footer>
+        )
+      };
